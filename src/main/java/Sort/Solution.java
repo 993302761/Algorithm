41ee145d4336e76cfbdb2fb339a18fbb909b3c9b
993302761 <993302761@qq.com>
2023-02-23 00:40:31 +0800

    存在重复元素

---
+++ src/main/java/Sort/Solution.java
@@ -0,0 +1,24 @@
+package Sort;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；如果数组中每个元素互不相同，返回 false 。
+ */
+public class Solution {
+
+    public static boolean containsDuplicate(int[] nums) {
+        Set s=new HashSet();
+        for (int a : nums) {
+            if (!s.add(a)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(containsDuplicate(new int[]{1,2,3,1}));
+    }
+}
