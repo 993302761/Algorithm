5c368981716ed93021babc0715a052a6ee6c9f1a
993302761 <993302761@qq.com>
2023-03-01 22:27:24 +0800

    跳跃游戏

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import SlidingWindow.Solution;
+import Greed.Solution;
 import Threads.FooBar;
 
 import java.util.HashSet;
@@ -6,7 +6,6 @@ import java.util.HashSet;
 public class Main {
     public static void main(String[] args) {
         Solution solution=new Solution();
-        System.out.println(solution.lengthOfLongestSubstring("dvdf"
-        ));
+        System.out.println(solution.canJump(new int[]{0}));
     }
 }
