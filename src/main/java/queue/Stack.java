c0f009b26f3b2d773eb6f6d8bb56373af9155f2f
993302761 <993302761@qq.com>
2023-03-21 00:31:03 +0800

    通过栈实现队列

---
+++ src/main/java/queue/Stack.java
@@ -0,0 +1,22 @@
+package queue;
+
+import java.util.ArrayDeque;
+
+public class Stack {
+
+    public static void main(String[] args) {
+        ArrayDeque deque1=new ArrayDeque();
+        ArrayDeque deque2=new ArrayDeque();
+        for (int i = 0; i < 10; i++) {
+            deque1.push(i);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            deque2.push(deque1.pop());
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(deque2.pop());
+        }
+    }
+}
